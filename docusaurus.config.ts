5176b6c6c27912336b8ca982faf84813b7f8b6c0
Adam Ross <66434612+adamross94@users.noreply.github.com>
2025-04-13 19:13:11 +0100

    Update docusaurus.config.ts
    onBrokenLinks: 'warn', // change from 'throw' to 'warn'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,8 +18,9 @@ const config: Config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn', // change from 'throw' to 'warn'
   onBrokenMarkdownLinks: 'warn',
+  onBrokenAnchors: 'warn', // optional but helps for anchor issues too
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
